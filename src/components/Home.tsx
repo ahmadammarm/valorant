b0d2d0ad0c94bf49c9cd8897d470a3b96a51b303
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2024-12-29 06:43:27 +0700

    feat: add lazy loading in homepage image

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="bg-gradient-to-b from-gray-900 to-gray-800 mt-5 md:mt-10">
       <div className="flex flex-col md:flex-row px-10 py-10 md:px-20 md:py-20">
             <div className="mt-20">
-                <img src="/valo.jpg" alt="" className="w-[40rem] rounded-lg border md:mr-10"/>
+                <img src="/valo.jpg" alt="" className="w-[40rem] rounded-lg border md:mr-10" loading="lazy"/>
             </div>
             <div className="mt-10 md:mt-36">
                 <h1 className="text-4xl font-bold text-center text-white">
